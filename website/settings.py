21dd70846dddc6e053860f7c4091442636f26213
jackliu612 <jackliu612@gmail.com>
2021-02-27 10:36:09 -0500

    fixing static

--- website/settings.py
+++ website/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
